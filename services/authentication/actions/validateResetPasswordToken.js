59967a2246c27f2945fa1a435133ba70419f64e7
Satish Gaikwad <satish@satishweb.com>
2017-02-27 23:02:21 +0530

    Removed mongoose ref

--- services/authentication/actions/validateResetPasswordToken.js
+++ services/authentication/actions/validateResetPasswordToken.js
@@ -4,7 +4,6 @@ var utils = require(__base + 'sharedlib/utils');
 var Locale = require(__base + 'sharedlib/formatter');
 var outputFormatter = new Locale(__dirname + '/../');
 var lodash = require('lodash');
-var mongoose = require('mongoose');
 var Promise = require('bluebird');
 var microtime = require('microtime');
 var Token = null;
@@ -58,7 +57,7 @@ function sendResponse(result, done) {
 module.exports = function(options) {
     var seneca = options.seneca;
     return function(args, done) {
-        Token = Token || mongoose.model('Tokens');
+        // Token = Token || mongoose.model('Tokens');
 
         // check if input token is valid
         utils.verifyTokenAndDecode(args.header.authorization)
@@ -70,12 +69,10 @@ module.exports = function(options) {
                 return sendResponse(response, done);
             })
             .catch(function(err) {
-                seneca.log.error('[ ' + process.env.SRV_NAME + ': ' + __filename.split('/').slice(-1) + ' ]', "ERROR" +
-                  " : ", err);
+                seneca.log.error('[ ' + process.env.SRV_NAME + ': ' + __filename.split('/').slice(-1) + ' ]', "ERROR" + " : ", err);
                 done(null, {
                     statusCode: 200,
-                    content: err.success === true || err.success === false ? err :
-                      utils.error(err.id || 400, err ? err.msg : 'Unexpected error', microtime.now())
+                    content: err.success === true || err.success === false ? err : utils.error(err.id || 400, err ? err.msg : 'Unexpected error', microtime.now())
                 });
             });
     };
