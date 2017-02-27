8e12939e302e71dd569ae740499edfe2c3a971af
Satish Gaikwad <satish@satishweb.com>
2017-02-27 16:12:05 +0530

    Updated cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+
+cleanDir() {
+	rm -rf index.js translations.json shared*
+}
 rm -rf containers/*
 cd services/api/
-rm -rf index.js translations.json shared* api/email api/authentication api/bootstrap api/invitations api/teams api/ugrp
-cd ../email
-rm -rf index.js translations.json shared*
-cd ../authentication
-rm -rf index.js translations.json shared*
-cd ../invitations
-rm -rf index.js translations.json shared*
-cd ../teams
-rm -rf index.js translations.json shared*
-cd ../ugrp
-rm -rf index.js translations.json shared*
+rm -rf index.js translations.json shared* api/email api/authentication api/bootstrap api/invitations api/organizations api/ugrp api/projects
+cd ../email && cleanDir
+cd ../authentication && cleanDir
+cd ../invitations && cleanDir
+cd ../organizations && cleanDir
+cd ../ugrp && cleanDir
+cd ../projects && cleanDir
+cd ../jobs && cleanDir
